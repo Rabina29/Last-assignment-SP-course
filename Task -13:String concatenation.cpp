970dedc1450c94fee336860dcb74626ed135e886
Rabina29 <mstrabinabegum029@gmail.com>
2024-12-08 05:41:50 -0800

    Add Create Task -13:String concatenation.cpp

---
+++ Task -13:String concatenation.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    char str1[40]="Hello ";
+    char str2[]="world";
+    int i=0,len=0,j=0;
+    while(str1[i]!='\0')
+    {
+        i++;
+        len++;
+    }
+    while(str2[j]!='\0')
+    {
+        str1[len+j]=str2[j];
+        j++;
+    }
+    printf(" %s\n",str1);
+    return 0;
+
+}
